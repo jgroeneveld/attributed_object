58dcff8c156ad65276749257ed3c5515e744aec4
Jaap Groeneveld <jaap.groeneveld@xing.com>
2016-12-23 09:49:17 +0100

    update benchmark to include coercion

--- benchmark_attributed_object.rb
+++ benchmark_attributed_object.rb
@@ -1,28 +1,28 @@
 require 'benchmark'
-require 'virtus'
-require 'dry/types'
+# require 'virtus'
+# require 'dry/types'
 require 'attributed_object'
 
-class VirtusValue
-  include Virtus.value_object
-
-  values do
-    attribute :id, Integer
-    attribute :claimed, Boolean
-    attribute :pro_coach, Boolean
-    attribute :url, String
-  end
-end
-
-class VirtusModel
-  include Virtus.model(:strict => true)
-
-  attribute :id, Integer
-  attribute :claimed, Boolean
-  attribute :pro_coach, Boolean
-  attribute :url, String
-end
-
+# class VirtusValue
+#   include Virtus.value_object
+#
+#   values do
+#     attribute :id, Integer
+#     attribute :claimed, Boolean
+#     attribute :pro_coach, Boolean
+#     attribute :url, String
+#   end
+# end
+#
+# class VirtusModel
+#   include Virtus.model(:strict => true)
+#
+#   attribute :id, Integer
+#   attribute :claimed, Boolean
+#   attribute :pro_coach, Boolean
+#   attribute :url, String
+# end
+#
 class AttrObj
   include AttributedObject
 
@@ -32,12 +32,22 @@ class AttrObj
   attribute :url, :string, disallow: nil
 end
 
-class DryValue < Dry::Types::Value
-  attribute :id, 'strict.int'
-  attribute :claimed, "strict.bool"
-  attribute :pro_coach, "strict.bool"
-  attribute :url, "strict.string"
+class AttrObjCoerce
+  include AttributedObject
+  attributed_object type_check: :coerce
+  
+  attribute :id, :integer, disallow: nil
+  attribute :claimed, :boolean, disallow: nil
+  attribute :pro_coach, :boolean, disallow: nil
+  attribute :url, :string, disallow: nil
 end
+#
+# class DryValue < Dry::Types::Value
+#   attribute :id, 'strict.int'
+#   attribute :claimed, "strict.bool"
+#   attribute :pro_coach, "strict.bool"
+#   attribute :url, "strict.string"
+# end
 
 class Poro
   attr_reader :id
@@ -55,27 +65,39 @@ end
 
 iterations = 10_000
 Benchmark.bm(27) do |bm|
-  bm.report('Virtus Value') do
-    iterations.times do
-      VirtusValue.new(id: 1, claimed: true, pro_coach: true, url: 'http://google.de')
-    end
-  end
-
-  bm.report('DryValue') do
-    iterations.times do
-      DryValue.new(id: 1, claimed: true, pro_coach: true, url: 'http://google.de')
-    end
-  end
-
-  bm.report('Virtus Model') do
-    iterations.times do
-      VirtusModel.new(id: 1, claimed: true, pro_coach: true, url: 'http://google.de')
-    end
-  end
+  # bm.report('Virtus Value') do
+  #   iterations.times do
+  #     VirtusValue.new(id: 1, claimed: true, pro_coach: true, url: 'http://google.de')
+  #   end
+  # end
+  #
+  # bm.report('DryValue') do
+  #   iterations.times do
+  #     DryValue.new(id: 1, claimed: true, pro_coach: true, url: 'http://google.de')
+  #   end
+  # end
+  #
+  # bm.report('Virtus Model') do
+  #   iterations.times do
+  #     VirtusModel.new(id: 1, claimed: true, pro_coach: true, url: 'http://google.de')
+  #   end
+  # end
 
   bm.report('AttributedObject') do
     iterations.times do
       AttrObj.new(id: 1, claimed: true, pro_coach: true, url: 'http://google.de')
+    end
+  end
+
+  bm.report('AttributedObjectCoerce Match') do
+    iterations.times do
+      AttrObjCoerce.new(id: 1, claimed: true, pro_coach: true, url: 'http://google.de')
+    end
+  end
+
+  bm.report('AttributedObjectCoerce All Strings') do
+    iterations.times do
+      AttrObjCoerce.new(id: '1', claimed: '1', pro_coach: 'true', url: 'http://google.de')
     end
   end
 
